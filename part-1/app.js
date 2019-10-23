cb125351905fa0db2fb9614231a1ab8e58ba1d78
George Tzannetos <georgios.tzannetos@tds.fujitsu.com>
2019-10-23 20:08:29 +0200

    Last change

--- part-1/app.js
+++ part-1/app.js
@@ -1,34 +1,57 @@
-const vid = document.querySelector('video');
-navigator.mediaDevices.getUserMedia({video: true}) // request cam
-.then(stream => {
-  vid.srcObject = stream; // don't use createObjectURL(MediaStream)
-  return vid.play(); // returns a Promise
-})
-.then(()=>{ // enable the button
-  const btn = document.querySelector('button');
-  btn.disabled = false;
-  btn.onclick = e => {
-    takeASnap()
-    .then(download);
-  };
-});
+// Set constraints for the video stream
+var constraints = { video: { facingMode: "user" }, audio: false };
+var track = null;
 
-function takeASnap(){
-  const canvas = document.createElement('canvas'); // create a canvas
-  const ctx = canvas.getContext('2d'); // get its context
-  canvas.width = vid.videoWidth; // set its size to the one of the video
-  canvas.height = vid.videoHeight;
-  ctx.drawImage(vid, 0,0); // the video
-  return new Promise((res, rej)=>{
-    canvas.toBlob(res, 'image/jpeg'); // request a Blob from the canvas
-  });
+// Define constants
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger");
+
+// Access the device camera and stream to cameraView
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+            track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+        })
+        .catch(function(error) {
+            console.error("Oops. Something is broken.", error);
+        });
 }
-function download(blob){
-  // uses the <a download> to download a Blob
-  let a = document.createElement('a'); 
-  a.href = URL.createObjectURL(blob);
-  a.download = 'screenshot.jpg';
-  document.body.appendChild(a);
-  a.click();
+
+// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/jpeg");
+       
+    var ajax = new XMLHttpRequest();
+    ajax.open("POST",'testSave.php',false);
+    ajax.setRequestHeader('Content-type','application/upload');
+    ajax.send(cameraOutput.src);
+    
+    cameraOutput.classList.add("taken");
+    console.log(cameraOutput)
+    // track.stop();
+};
+
+
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
+
+
+// Install ServiceWorker
+if ('serviceWorker' in navigator) {
+  console.log('CLIENT: service worker registration in progress.');
+  navigator.serviceWorker.register( '/camera-app/part-2/sw.js' , { scope : ' ' } ).then(function() {
+    console.log('CLIENT: service worker registration complete.');
+  }, function() {
+    console.log('CLIENT: service worker registration failure.');
+  });
+} else {
+  console.log('CLIENT: service worker is not supported.');
 }
 
